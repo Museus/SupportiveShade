2163c86d3de020c19abce2be133aa4740289ae8c
Museus <museus@protonmail.com>
2024-05-06 11:30:29 -0400

    build: add -d flag to deploy/start to mount app directory in container

--- deploy/start.sh
+++ deploy/start.sh
@@ -2,10 +2,12 @@
 
 unset -v
 REBUILD_IMAGE="false"
+DEVELOPMENT="false"
 
-while getopts b opt; do
+while getopts bd opt; do
 	case "$opt" in
 		b) REBUILD_IMAGE="true";;
+		d) DEVELOPMENT="true";;
 		*)
 			echo "Unknown flag" >&2
 			exit 1
@@ -14,10 +16,17 @@ done
 
 shift "$(( OPTIND -1 ))"
 
+COMMAND_STRING='BUILD_TIMESTAMP=$(date -u --rfc-3339=seconds) docker compose'
+
+if $DEVELOPMENT; then
+	COMMAND_STRING+=' -f compose.yml -f dev.compose.yml'
+fi
+
+COMMAND_STRING+=' up -d'
 
 if $REBUILD_IMAGE; then
-	BUILD_TIMESTAMP=$(date -u --rfc-3339=seconds) docker compose up -d --build
-else
-	BUILD_TIMESTAMP=$(date -u --rfc-3339=seconds) docker compose up -d
+	COMMAND_STRING+=' --build'
 fi
+
+eval "$COMMAND_STRING"
 
