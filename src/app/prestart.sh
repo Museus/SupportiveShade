7c9930673f6ce7c332cdfea610ab6ffa629b8e7a
Museus <museus@protonmail.com>
2024-05-06 12:00:40 -0400

    build: add -l flag to src/app/start to run outside of docker

--- src/app/prestart.sh
+++ src/app/prestart.sh
@@ -1,2 +1,25 @@
-echo "No prestart setup required..."
+shift "$(( OPTIND -1 ))"
+
+if $LOCAL; then
+	echo "You're running Supportive Shade locally."
+
+	echo "Sourcing virtual env"
+	source ../../.venv/bin/activate
+
+	echo "Sourcing deployment environment"
+	source ../../deploy/.env
+
+	echo "Linking config to /etc/supportive_shade/config.toml"
+	mkdir -p /etc/supportive_shade
+	ln -s $(readlink -f ../../deploy/config.toml) /etc/supportive_shade/config.toml
+
+	echo "Making data directory at /var/lib/supportive_shade"
+	mkdir -p /var/lib/supportive_shade
+
+	echo "Setup complete!"
+else
+	echo "Sourcing virtual env"
+	source /venv/bin/activate
+
+fi
 
--- src/app/start.sh
+++ src/app/start.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-source /venv/bin/activate
+unset -v
+LOCAL="false"
 
-. prestart.sh
+while getopts l opt; do
+	case "$opt" in
+		l) LOCAL="true";;
+		*)
+			echo "Unknown flag" >&2
+			exit 1
+	esac
+done
+
+shift "$(( OPTIND -1 ))"
+
+source ./prestart.sh
 
 echo "-- Starting Supportive Shade..."
 
-python3 main.py
+API_TOKEN=$API_TOKEN python3 main.py
 
